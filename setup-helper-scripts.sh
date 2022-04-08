7f53056cc7dabf7dfc3ef1f29ed7e811a1d6153a
Neil Docherty <n.docherty@cabi.org>
2022-04-08 10:06:32 +0100

    Include production user creation script in exec

--- setup-helper-scripts.sh
+++ setup-helper-scripts.sh
@@ -3,6 +3,7 @@ echo Set scripts to be executable
 chmod +x helpers/admin-create.sh
 chmod +x helpers/admin-delete.sh
 chmod +x helpers/user-create.sh
+chmod +x helpers/user-create-production.sh
 chmod +x helpers/user-delete.sh
 chmod +x helpers/refresh.sh
 chmod +x helpers/build-agent-service-account-create.sh
