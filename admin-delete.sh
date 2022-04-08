ff5246d9866d051f835074156277b64282bae09c
Neil Docherty <n.docherty@cabi.org>
2022-04-08 10:43:00 +0100

    More complete delete user

--- admin-delete.sh
+++ admin-delete.sh
@@ -3,4 +3,5 @@
 kubectl config delete-context $1-admin-context
 kubectl config delete-user $1-admin
 
+kubectl delete clusterrolebinding -n kube-system admin-$1
 kubectl delete clusterrolebinding -n kube-system admin-$1-admin
--- user-delete.sh
+++ user-delete.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [ "$2" != "" ]; then
-namespace=$2
-else
-namespace=development
-fi
+kubectl delete rolebinding -n development pod-reader-$1
+kubectl delete rolebinding -n staging pod-basic-reader-$1
+kubectl delete rolebinding -n production pod-basic-reader-$1
 
+kubectl delete clusterrolebinding -n kube-system admin-$1
+kubectl delete clusterrolebinding -n kube-system admin-$1-admin
+
+kubectl config delete-context $1-admin-context
 kubectl config delete-context $1-context
 kubectl config delete-user $1
-
-kubectl delete rolebinding -n $namespace pod-reader-$1
-kubectl delete rolebinding -n staging pod-reader-$1
+kubectl config delete-user $1-admin
 
 kubectl delete ns sandbox-$1
